40e9cf2937abdd706538a05f52e23c40ec505eec
JyuKa <yoyksks3@gmail.com>
2021-12-21 23:00:59 +0900

    공통된 응답 형식을 위한 ApiDataResponse 추가

--- pocketmark_backend/src/main/java/com/example/pocketmark/dto/UserReq.java
+++ pocketmark_backend/src/main/java/com/example/pocketmark/dto/UserReq.java
@@ -1,5 +1,12 @@
 package com.example.pocketmark.dto;
 
+import lombok.Data;
+
+@Data
 public class UserReq {
+
+    private String email;
+    private String pw;
+    private String nickName;
     
 }
